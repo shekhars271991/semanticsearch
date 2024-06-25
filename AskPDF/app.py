11a49fba40e0100d80eca888da2eb357c889200d
Shekhar Suman <shekhar_suman@outlook.com>
2024-06-25 09:03:37 +0530

    Working streamlit app

--- AskPDF/app.py
+++ AskPDF/app.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import numpy as np
 from sentence_transformers import SentenceTransformer
 import redis
@@ -5,10 +6,9 @@ from redis.commands.search.query import Query
 from openai import OpenAI
 from my_secrets import OPENAI_API_KEY 
 
-
-def search_similar_chunks(query, top_k=5, redis_host='localhost', redis_port=6379):
+def search_similar_chunks(model, query, top_k=5, redis_host='localhost', redis_port=6379):
     redis_client = redis.Redis(host=redis_host, port=redis_port, db=0)
-    model = SentenceTransformer('all-MiniLM-L6-v2')
+   
     
     query_embedding = model.encode(query)
     vector = np.array(query_embedding, dtype=np.float32).tobytes()
@@ -22,14 +22,11 @@ def search_similar_chunks(query, top_k=5, redis_host='localhost', redis_port=637
 
     matching_chunks = []
     for doc in results.docs:
-        # print(doc)
         matching_chunks.append(doc.chunk) 
     context = "\n\n".join(matching_chunks)
     return context
-    # return matching_chunks
 
 def ask_openai(context, question):
-    # Define parameters for the completion call
     template = """
     Answer the question based on the context below. If you can't 
     answer the question, reply "I don't know".
@@ -41,19 +38,25 @@ def ask_openai(context, question):
     prompt = template.format(context=context, question=question)
     client = OpenAI(api_key=OPENAI_API_KEY)
     chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": prompt,
-        }
-    ],
-    model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="gpt-3.5-turbo",
     )
-    print(chat_completion.choices[0].message.content)
+    return chat_completion.choices[0].message.content
 
+def main():
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    st.title("Question Answering App")
+    query = st.text_input("Enter your question:")
+    if st.button("Get Answer"):
+        context = search_similar_chunks(model, query)
+        answer = ask_openai(context, query)
+        st.subheader("Answer:")
+        st.write(answer)
 
-query = "tell me about proximity graph"
-context = search_similar_chunks(query)
-
-ask_openai(context,query)
-# print(context)
+if __name__ == "__main__":
+    main()
